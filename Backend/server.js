e0b4445f1fd0ec7bea3288b2b7876b6d3db1fdf1
iOmKumar30 <iomkumar03@outlook.com>
2024-08-22 03:11:08 +0530

    First Commit

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ const http = require("http");
 const WebSocket = require("ws");
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
@@ -29,13 +29,17 @@ const isFolderWritable = async (folderPath) => {
 
 app.post("/api/extract", async (req, res) => {
   const { link, startInSeconds, endInSeconds, folderPath } = req.body;
-  console.log(`Received request for video: ${link} from ${startInSeconds} to ${endInSeconds}`);
+  console.log(
+    `Received request for video: ${link} from ${startInSeconds} to ${endInSeconds}`
+  );
   console.log(`Download folder path: ${folderPath}`);
 
   try {
     await isFolderWritable(folderPath);
   } catch (error) {
-    return res.status(500).json({ error: `Error checking folder permissions: ${error.message}` });
+    return res
+      .status(500)
+      .json({ error: `Error checking folder permissions: ${error.message}` });
   }
 
   try {
@@ -55,10 +59,14 @@ app.post("/api/extract", async (req, res) => {
 
     const ytDlpProcess = spawn("yt-dlp", ["-f", "136+140", "-o", "-", link]);
     const ffmpegProcess = spawn("ffmpeg", [
-      "-i", "pipe:0",
-      "-ss", startInSeconds,
-      "-to", endInSeconds,
-      "-c", "copy",
+      "-i",
+      "pipe:0",
+      "-ss",
+      startInSeconds,
+      "-to",
+      endInSeconds,
+      "-c",
+      "copy",
       segmentPath,
     ]);
 
@@ -66,7 +74,7 @@ app.post("/api/extract", async (req, res) => {
 
     ytDlpProcess.stderr.on("data", (data) => {
       console.error(`yt-dlp stderr: ${data}`);
-      wss.clients.forEach(client => {
+      wss.clients.forEach((client) => {
         if (client.readyState === WebSocket.OPEN) {
           client.send(`yt-dlp stderr: ${data}`);
         }
@@ -75,7 +83,7 @@ app.post("/api/extract", async (req, res) => {
 
     ffmpegProcess.stderr.on("data", (data) => {
       console.error(`ffmpeg stderr: ${data}`);
-      wss.clients.forEach(client => {
+      wss.clients.forEach((client) => {
         if (client.readyState === WebSocket.OPEN) {
           client.send(`ffmpeg stderr: ${data}`);
         }
@@ -84,18 +92,20 @@ app.post("/api/extract", async (req, res) => {
 
     ffmpegProcess.on("error", (error) => {
       console.error(`Error during ffmpeg processing: ${error.message}`);
-      wss.clients.forEach(client => {
+      wss.clients.forEach((client) => {
         if (client.readyState === WebSocket.OPEN) {
           client.send(`Error during ffmpeg processing: ${error.message}`);
         }
       });
-      res.status(500).json({ error: `Error during ffmpeg processing: ${error.message}` });
+      res
+        .status(500)
+        .json({ error: `Error during ffmpeg processing: ${error.message}` });
     });
 
     ffmpegProcess.on("close", (code) => {
       if (code === 0) {
         console.log(`Segment created: ${segmentPath}`);
-        wss.clients.forEach(client => {
+        wss.clients.forEach((client) => {
           if (client.readyState === WebSocket.OPEN) {
             client.send(`Download complete: ${segmentPath}`);
           }
@@ -103,7 +113,9 @@ app.post("/api/extract", async (req, res) => {
         res.json({ downloadLink: `/downloads/${videoId}_segment.mp4` });
       } else {
         console.error(`ffmpeg process exited with code ${code}`);
-        res.status(500).json({ error: `ffmpeg process exited with code ${code}` });
+        res
+          .status(500)
+          .json({ error: `ffmpeg process exited with code ${code}` });
       }
     });
   } catch (error) {
